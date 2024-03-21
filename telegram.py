5d001ca16a794cb541206cf1a88a7ca222a41c48
jmpark1115 <jmpark1115@gmail.com>
2024-03-21 12:48:16 +0900

    first

---
+++ telegram.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+import requests
+import json
+
+API_URL = "https://api.telegram.org/bot"
+
+class Telegram(object):
+
+    def __init__(self, key, chat_id) :
+        self.key = key
+        self.chat_id = chat_id
+        self.markdown = False
+        self.html = False
+
+    def get_me(self):
+        url = API_URL + self.key + "/getMe"
+        resp = requests.get(url)
+        return resp.json()
+
+    def get_updates(self):
+        url = API_URL + self.key + "/getUpdates"
+        resp = requests.get(url)
+        return resp.json()
+
+    def message(self, message):
+        url = API_URL + self.key + "/sendMessage"
+        params = {
+                  "chat_id": self.chat_id,
+                  "text": message
+        }
+        resp = requests.post(url, params=params)
+        content = resp.json()
+        return content
+
+if __name__ == "__main__":
+    # Load Config File
+    chat_id = ''
+    chat_token = ''
+
+    tg = Telegram(chat_token, chat_id)
+    result = tg.message("welcome to my telegram message service !!")
+    print(result)
---
+++ ticker.py
@@ -0,0 +1,23 @@
+"""
+빗썸과 코인원의 시세를 조회한다
+"""
+import requests
+import json
+
+print("빗썸과 코인원의 시세를 조회합니다")
+
+url = "https://api.bithumb.com/public/ticker/ALL"
+res = requests.get(url)
+ticker = res.json()
+bBTC = int(ticker['data']['BTC']['closing_price'])
+print("Bithumb BTC(%.2f)" % bBTC )
+
+#coinone
+url = 'https://api.coinone.co.kr/ticker'
+data = {'currency' : 'all'}
+res = requests.get(url, params=data)
+ticker = res.json()
+temp = ticker['btc']['last']
+cBTC = int(float(ticker['btc']['last'])) # 소숫점이 있어 float 으로 변환 후 int 함.
+print("Coinone BTC(%.2f)" % cBTC)
+
