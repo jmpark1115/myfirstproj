4012b830903d65315ee2c91c767b8f803a31f1a1
jmpark1115 <jmpark1115@gmail.com>
2024-03-25 17:12:37 +0900

    btc 시세를 5초마다 telegram 으로 발송

--- ticker.py
+++ ticker.py
@@ -1,23 +1,41 @@
 """
-빗썸과 코인원의 시세를 조회한다
+빗썸의 시세를 5초마다 조회한다.
+조회한 결과를 텔레그램으로 알려준다.
 """
 import requests
-import json
+import time
+import datetime
+
+API_URL = "https://api.telegram.org/bot"
+chat_id = ''
+chat_token = ''
 
 print("빗썸과 코인원의 시세를 조회합니다")
 
-url = "https://api.bithumb.com/public/ticker/ALL"
-res = requests.get(url)
-ticker = res.json()
-bBTC = int(ticker['data']['BTC']['closing_price'])
-print("Bithumb BTC(%.2f)" % bBTC )
+def seek_coin_value(coin='BTC'):
+    url = "https://api.bithumb.com/public/ticker/" + coin +'_KRW'
+    res = requests.get(url)
+    ticker = res.json()
+    coinValue = float(ticker['data']['closing_price'])
+    return coinValue
 
-#coinone
-url = 'https://api.coinone.co.kr/ticker'
-data = {'currency' : 'all'}
-res = requests.get(url, params=data)
-ticker = res.json()
-temp = ticker['btc']['last']
-cBTC = int(float(ticker['btc']['last'])) # 소숫점이 있어 float 으로 변환 후 int 함.
-print("Coinone BTC(%.2f)" % cBTC)
+def sendMessage(message):
+    url = API_URL + chat_token + "/sendMessage"
+    params = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    resp = requests.post(url, params=params)
+    content = resp.json()
+    return content
+
+for i in range(10):
+    coinValue = seek_coin_value('BTC')
+    if coinValue:
+        currentTime = datetime.datetime.now()
+        # 현재 일시를 yyyy-mm-dd HH:MM:SS 형식으로 표기
+        formattedTime = currentTime.strftime("%Y-%m-%d %H:%M:%S")
+        sendMessage("[{}] {:,.0f}" .format(formattedTime, coinValue))
+    time.sleep(5)
+
 
